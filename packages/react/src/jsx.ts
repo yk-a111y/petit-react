f8abbe9e2a916d598d982e2cdb4889e469f109bb
Kai <yk19980811@163.com>
2025-03-11 22:03:26 +0800

    chore: pnpm global link

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -58,4 +58,33 @@ export const jsx = (
   return ReactElement(type, key, ref, props);
 };
 
-export const jsxDev = jsx;
+export const jsxDEV =  (
+  type: ElementType,
+  config: any,
+) => {
+  let key: Key = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  for (const prop in config) {
+    const val = config[prop];
+    if (prop === 'key') {
+      if (val !== undefined) {
+        key = '' + val;
+      }
+    }
+    if (prop === 'ref') {
+      if (val !== undefined) {
+        ref = val;
+      }
+    }
+
+    // *安全调用，防止config由Object.create(null)创建，无原型链导致报错
+    // *只复制属于config对象自身的属性到props对象中
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = val;
+    }
+  }
+
+  return ReactElement(type, key, ref, props);
+};;
---
+++ react-demo/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const jsx = (
+  <div>
+    <span>text</span>
+  </div>
+)
+
+console.log("🚀 ~ React:", React)
+console.log("🚀 ~ jsx:", jsx)
+
